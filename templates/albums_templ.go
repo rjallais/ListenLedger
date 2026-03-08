58b276333e6720c58c031aceeae4f77c24d4fab7
rjallais <rfj.dev@outlook.com>
2026-03-08 17:04:32 -0300

    chore: refresh templ generated imports

--- templates/albums_templ.go
+++ templates/albums_templ.go
@@ -5,12 +5,10 @@ package templates
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"fmt"
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "fmt"
 
 // AlbumsPage renders the albums collection page with lazy-loaded sections
 func AlbumsPage(fullCount, processedCount, waitingCount int) templ.Component {
--- templates/artists_templ.go
+++ templates/artists_templ.go
@@ -5,12 +5,10 @@ package templates
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"fmt"
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "fmt"
 
 // ArtistsPage renders the artists page with pagination
 func ArtistsPage(artists []Artist, rockMetalCount, everythingElseCount int, currentGenre string, waitingCount int, pagination Pagination) templ.Component {
